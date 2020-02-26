67376bb69d6d6c5c1fc33929a84fc264b525b572
Andrew Appleby <andrewappleby22@gmail.com>
2020-02-25 18:09:36 -0800

    Updates

---
+++ precache-manifest.ccb7c60ee8a061a3e8b7bed05f667c8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "babd9c1d74e53f8a509feee744272037",
+    "url": "/window-splitter/index.html"
+  },
+  {
+    "revision": "7398e5430d20aadb3975",
+    "url": "/window-splitter/static/css/main.65a6af82.chunk.css"
+  },
+  {
+    "revision": "84287aee4c8d8739a1f1",
+    "url": "/window-splitter/static/js/2.232d8e9a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/window-splitter/static/js/2.232d8e9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7398e5430d20aadb3975",
+    "url": "/window-splitter/static/js/main.2d9b4cdd.chunk.js"
+  },
+  {
+    "revision": "edadf398405ef7a7148f",
+    "url": "/window-splitter/static/js/runtime-main.21a02573.js"
+  }
+]);
